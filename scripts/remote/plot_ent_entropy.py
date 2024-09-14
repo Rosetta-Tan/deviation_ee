62ef229ce43c1b3e64fbe5a00e82c1e5e4373656
Rosetta-Tan <rosetta_tan@outlook.com>
2024-09-14 10:54:09 -0400

    back up results for old sqrt_coeff data run

--- scripts/remote/plot_ent_entropy.py
+++ scripts/remote/plot_ent_entropy.py
@@ -9,7 +9,7 @@ plt.style.use('../../figures/norm.mplstyle')
 Ls = range(12, 25, 2)
 seeds = list(range(20))
 tols = [0.1, 0.01, 0.001]
-obs_dir = '/n/home01/ytan/scratch/deviation_ee/old/obs_syk'
+obs_dir = '/n/home01/ytan/scratch/deviation_ee/obs_syk'
 
 # get simulation data
 # ees: {tol1: [[seed1, seed2, ...], ...], tol2: [[seed1, ...], ...], ...}
@@ -54,10 +54,10 @@ for L in Ls:
                 if int(row[0]) == L:
                     ees_page.append(float(row[1]))
                     break
-ees_page = np.array(ees_page).reshape(-1, 1).repeat(len(seeds), axis=1)
+ees_page = np.array(ees_page).reshape(-1, 1)  # reshape to (len(Ls), 1)
 
 deltas = {}
-with open('ent_entropy_old.txt', 'w') as f:
+with open('ent_entropy.txt', 'w') as f:
     for tol in tols:
         # deltas[tol] = np.array(ees_page)-np.average(ees[tol], axis=1)
         deltas[tol] = np.asarray(ees_page) - ees[tol]
@@ -82,15 +82,15 @@ with open('ent_entropy_old.txt', 'w') as f:
 colors = plt.colormaps.get_cmap('PuBu')
 bright_colors = [colors(i) for i in [0.3,0.6,0.9]]
 
-# fig, ax = plt.subplots()
-# for i, tol in enumerate(tols):
-#     ax.errorbar(Ls, np.average(deltas[tol], axis=1), yerr=np.std(deltas[tol], axis=1),
-#                 fmt='-o', capsize=5, label=f'tol={tol}',
-#                 markeredgecolor='black', markeredgewidth=1,
-#                 color=bright_colors[i])
-#     ax.plot(Ls, deltas_thermal[tol], '-o', color=bright_colors[i])
-# ax.set_xlabel('L')
-# ax.set_xticks(Ls)
-# ax.set_ylabel('$\Delta S$')
-# ax.legend()
-# fig.savefig('../../figures/ent_entropy.pdf', bbox_inches='tight')
+fig, ax = plt.subplots()
+for i, tol in enumerate(tols):
+    ax.errorbar(Ls, np.average(deltas[tol], axis=1), yerr=np.std(deltas[tol], axis=1),
+                fmt='-o', capsize=5, label=f'tol={tol}',
+                markeredgecolor='black', markeredgewidth=1,
+                color=bright_colors[i])
+    ax.plot(Ls, np.average(deltas[tol], axis=1), '-o', color=bright_colors[i])
+ax.set_xlabel('L')
+ax.set_xticks(Ls)
+ax.set_ylabel('$\Delta S$')
+ax.legend()
+fig.savefig('../../figures/ent_entropy.pdf', bbox_inches='tight')
